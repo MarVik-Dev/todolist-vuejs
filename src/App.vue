bec9a0402821b2809cc3629b5cc56e98edf92df8
MarVik <55230624+marvikfr@users.noreply.github.com>
2020-09-18 18:17:26 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,16 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <header-top></header-top>
     <mon-contenu></mon-contenu>
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
 import Header from "./components/Header"
 import Contenu from "./components/Contenu"
 
 export default {
   name: 'App',
   components: {
-    // HelloWorld,
     'header-top': Header,
     'mon-contenu': Contenu
   }
